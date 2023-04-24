2cf2c33c8f743fc455429e535aa252e3128df92b
IHEAMARAME EMMANUEL UCHECHUKWU <45761265+mayortechyx1@users.noreply.github.com>
2023-04-24 23:48:43 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,6 +2,8 @@
 
 /**
  * is_printable - Evaluates if a char is printable
+ * joint team between akpovwovwo and uchechukwu
+ * perfectly done
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
@@ -16,23 +18,25 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * joint team between akpovwovwo and uchechukwu
+ * perfectly done
  * @buffer: Array of char
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int v)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+	buffer[v++] = '\\';
+	buffer[v++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[v++] = map_to[ascii_code / 16];
+	buffer[v] = map_to[ascii_code % 16];
 
 	return (3);
 }
@@ -40,6 +44,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
+ * joint team between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -55,6 +61,8 @@ int is_digit(char c)
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
+ * joint team between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: Casted value of num
  */
@@ -70,6 +78,8 @@ long int convert_size_number(long int num, int size)
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
+ * joint team between akpovwovwo and uchechukwu
+ * perfectly done
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
  *
