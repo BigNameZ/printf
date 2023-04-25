9c0b2c06e2d9252e3f47be3fa15479ebf1a64643
IHEAMARAME EMMANUEL UCHECHUKWU <45761265+mayortechyx1@users.noreply.github.com>
2023-04-25 08:40:18 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -2,22 +2,24 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a strings
- * joint team between akpovwovwo and uchechukwu
+ * handle_write_char - Prints a string
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
- * @c: char type
+ * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
  * @width: get width.
- * @precision: precision specifiers
+ * @precision: precision specifier
  * @size: Size specifier
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
-	int v = 0;
+	int i = 0;
 	char padd = ' ';
 
 	UNUSED(precision);
@@ -26,20 +28,20 @@ int handle_write_char(char c, char buffer[],
 	if (flags & F_ZERO)
 		padd = '0';
 
-	buffer[v++] = c;
-	buffer[v] = '\0';
+	buffer[i++] = c;
+	buffer[i] = '\0';
 
 	if (width > 1)
 	{
 		buffer[BUFF_SIZE - 1] = '\0';
-		for (v = 0; v < width - 1; v++)
-			buffer[BUFF_SIZE - v - 2] = padd;
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFF_SIZE - i - 2] = padd;
 
 		if (flags & F_MINUS)
 			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - v - 1], width - 1));
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
-			return (write(1, &buffer[BUFF_SIZE - v - 1], width - 1) +
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
 					write(1, &buffer[0], 1));
 	}
 
@@ -49,7 +51,7 @@ int handle_write_char(char c, char buffer[],
 /************************* WRITE NUMBER *************************/
 /**
  * write_number - Prints a string
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
  * @is_negative: Lista of arguments
  * @ind: char types.
@@ -58,6 +60,8 @@ int handle_write_char(char c, char buffer[],
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: Number of chars printed.
  */
@@ -85,7 +89,7 @@ int write_number(int is_negative, int ind, char buffer[],
 /**
  * write_num - Write a number using a bufffer
  * @ind: Index at which the number starts on the buffer
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
  * @buffer: Buffer
  * @flags: Flags
@@ -93,7 +97,9 @@ int write_number(int is_negative, int ind, char buffer[],
  * @prec: Precision specifier
  * @length: Number length
  * @padd: Pading char
- * @extra_c: Extra chars
+ * @extra_c: Extra char
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: Number of printed chars.
  */
@@ -101,7 +107,7 @@ int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
 {
-	int v, padd_start = 1;
+	int i, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 		return (0); /* printf(".0d", 0)  no char is printed */
@@ -115,26 +121,26 @@ int write_num(int ind, char buffer[],
 		length++;
 	if (width > length)
 	{
-		for (v = 1; v < width - length + v; v++)
-			buffer[v] = padd;
-		buffer[v] = '\0';
+		for (i = 1; i < width - length + 1; i++)
+			buffer[i] = padd;
+		buffer[i] = '\0';
 		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) + write(1, &buffer[1], v - 1));
+			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[1], v - 1) + write(1, &buffer[ind], length));
+			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
 		}
 		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 		{
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
-			return (write(1, &buffer[padd_start], v - padd_start) +
+			return (write(1, &buffer[padd_start], i - padd_start) +
 				write(1, &buffer[ind], length - (1 - padd_start)));
 		}
 	}
@@ -146,13 +152,15 @@ int write_num(int ind, char buffer[],
 /**
  * write_unsgnd - Writes an unsigned number
  * @is_negative: Number indicating if the num is negative
- * joint team between akpovwovwo and uchechukwu
- * perfectly done
  * @ind: Index at which the number starts in the buffer
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  * @buffer: Array of chars
  * @flags: Flags specifiers
  * @width: Width specifier
  * @precision: Precision specifier
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  * @size: Size specifier
  *
  * Return: Number of written chars.
@@ -162,7 +170,7 @@ int write_unsgnd(int is_negative, int ind,
 	int flags, int width, int precision, int size)
 {
 	/* The number is stored at the bufer's right and starts at position i */
-	int length = BUFF_SIZE - ind - 1, v = 0;
+	int length = BUFF_SIZE - ind - 1, i = 0;
 	char padd = ' ';
 
 	UNUSED(is_negative);
@@ -185,18 +193,18 @@ int write_unsgnd(int is_negative, int ind,
 
 	if (width > length)
 	{
-		for (v = 0; v < width - length; v++)
-			buffer[v] = padd;
+		for (i = 0; i < width - length; i++)
+			buffer[i] = padd;
 
-		buffer[v] = '\0';
+		buffer[i] = '\0';
 
 		if (flags & F_MINUS) /* Asign extra char to left of buffer [buffer>padd]*/
 		{
-			return (write(1, &buffer[ind], length) + write(1, &buffer[0], v));
+			return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
 		}
 		else /* Asign extra char to left of padding [padd>buffer]*/
 		{
-			return (write(1, &buffer[0], v) + write(1, &buffer[ind], length));
+			return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
 		}
 	}
 
@@ -207,13 +215,15 @@ int write_unsgnd(int is_negative, int ind,
  * write_pointer - Write a memory address
  * @buffer: Arrays of chars
  * @ind: Index at which the number starts in the buffer
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
- * @length: Length of numbers
+ * @length: Length of number
  * @width: Width specifier
  * @flags: Flags specifier
  * @padd: Char representing the padding
  * @extra_c: Char representing extra char
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  * @padd_start: Index at which padding should start
  *
  * Return: Number of written chars.
@@ -221,20 +231,20 @@ int write_unsgnd(int is_negative, int ind,
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start)
 {
-	int v;
+	int i;
 
 	if (width > length)
 	{
-		for (v = 3; v < width - length + 3; v++)
-			buffer[v] = padd;
-		buffer[v] = '\0';
+		for (i = 3; i < width - length + 3; i++)
+			buffer[i] = padd;
+		buffer[i] = '\0';
 		if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
 		{
 			buffer[--ind] = 'x';
 			buffer[--ind] = '0';
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) + write(1, &buffer[3], v - 3));
+			return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buffer */
 		{
@@ -242,7 +252,7 @@ int write_pointer(char buffer[], int ind, int length,
 			buffer[--ind] = '0';
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[3], v - 3) + write(1, &buffer[ind], length));
+			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
 		}
 		else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 		{
@@ -250,7 +260,7 @@ int write_pointer(char buffer[], int ind, int length,
 				buffer[--padd_start] = extra_c;
 			buffer[1] = '0';
 			buffer[2] = 'x';
-			return (write(1, &buffer[padd_start], v - padd_start) +
+			return (write(1, &buffer[padd_start], i - padd_start) +
 				write(1, &buffer[ind], length - (1 - padd_start) - 2));
 		}
 	}
