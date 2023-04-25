34d5ff3277622a4e058cb1f345f0e3dcb391109a
IHEAMARAME EMMANUEL UCHECHUKWU <45761265+mayortechyx1@users.noreply.github.com>
2023-04-25 08:34:49 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,9 +2,11 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
  * @c: Char to be evaluated.
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -18,36 +20,40 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
- * @buffer: Array of char
+ * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  */
-int append_hexa_code(char ascii_code, char buffer[], int v)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[v++] = '\\';
-	buffer[v++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
-	buffer[v++] = map_to[ascii_code / 16];
-	buffer[v] = map_to[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
 	return (3);
 }
 
 /**
  * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  */
 int is_digit(char c)
 {
@@ -61,7 +67,7 @@ int is_digit(char c)
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
- * joint team between akpovwovwo and uchechukwu
+ * its a joint project between akpovwovwo and uchechukwu
  * perfectly done
  *
  * Return: Casted value of num
@@ -78,10 +84,10 @@ long int convert_size_number(long int num, int size)
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
- * joint team between akpovwovwo and uchechukwu
- * perfectly done
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
+ * its a joint project between akpovwovwo and uchechukwu
+ * perfectly done
  *
  * Return: Casted value of num
  */
